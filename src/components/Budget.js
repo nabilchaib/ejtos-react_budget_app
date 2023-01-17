a6bf853d691b208916dd255997c6aec3752c9844
nabilchaib <nabilchaibdraa@gmail.com>
2023-01-17 09:40:06 -0500

    Budget app commit

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,15 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-
+    const { budget, dispatch } = useContext(AppContext);
+    const handleChange = (e) => {
+        const newBudget = e.target.value;
+        dispatch({ type: 'SET_BUDGET', payload: newBudget });
+    }
     return (
         <div className='alert alert-secondary'>
             <span>Budget: £{budget}</span>
+            <input type="number" className="form-control" placeholder="Enter new budget" onChange={handleChange} />
         </div>
     );
 };
